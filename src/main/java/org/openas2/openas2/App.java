9d914c35b7626c4ba04792cfb62d6471ee6989eb
charmsstyler <charmsstyler@gmail.com>
2015-05-22 16:17:19 -0400

    Frist Commit- Forked from http://sourceforge.net/p/openas2/code/?source=navbar;  Converted Project to Maven; added gitignore

---
+++ src/main/java/org/openas2/openas2/App.java
@@ -0,0 +1,13 @@
+package org.openas2.openas2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
